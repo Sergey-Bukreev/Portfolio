3651dd630f38c3daa1ec2dad9e6ed3c633a48c0d
BukreevSergey <bukreevwork2@gmail.com>
2023-11-09 22:37:12 +0200

    make Header

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,13 @@
+import Icon from "../icon/Icon";
+
+export const Logo = () => {
+    return (
+
+        <a href={""}>
+            <Icon iconId={"codeSvg"}/>
+        </a>
+
+    );
+};
+
+export default Logo;
