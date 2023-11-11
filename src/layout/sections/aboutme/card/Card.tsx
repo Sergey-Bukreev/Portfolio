182d3640bea4d9de93177c99846cac2613ed3ecd
BukreevSergey <bukreevwork2@gmail.com>
2023-11-11 16:12:42 +0200

    add section About me

---
+++ src/layout/sections/aboutme/card/Card.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from "styled-components";
+import Icon from "../../../../components/icon/Icon";
+import {FlexWrapper} from "../../../../components/FlexWrapper";
+type CardPropsType ={
+    speciality: string
+    description: string
+    place: string
+    period: string
+}
+
+ export const Card = (props:CardPropsType) => {
+    return (
+        <StyledCard>
+            <FlexWrapper align={"center"}>
+                <UpElementTitle>{props.speciality}</UpElementTitle>
+                <UpElementText>{props.description}</UpElementText>
+            </FlexWrapper>
+            <FlexWrapper align={"center"}>
+                <Icon iconId={"building"} />
+                <DownElementTitle>{props.place}</DownElementTitle>
+                <Icon iconId={"calendar"} />
+                <DownElementText>{props.period}</DownElementText>
+            </FlexWrapper>
+        </StyledCard>
+    );
+};
+
+const StyledCard = styled.div`
+border: black 1px solid;
+  min-width: 100vh;
+  
+`
+const UpElementTitle = styled.p``
+const UpElementText = styled.span`
+  font-size: 12px;
+`
+const DownElementTitle = styled.p`
+  font-size: 12px;
+`
+const DownElementText = styled.span`
+  font-size: 12px;
+`
