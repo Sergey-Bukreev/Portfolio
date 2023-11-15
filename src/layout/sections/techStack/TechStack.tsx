ffb49337365d99ffc5dc282fdcd654d0cd35cb15
BukreevSergey <bukreevwork2@gmail.com>
2023-11-15 14:08:22 +0200

    fix icons

--- src/layout/sections/techStack/TechStack.tsx
+++ src/layout/sections/techStack/TechStack.tsx
@@ -10,18 +10,18 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
             <SectionTitle>MY Tech Stack</SectionTitle>
             <Text>Technologies I’ve been working with recently</Text>
             <FlexWrapper wrap={"wrap"} justify={"space-between"}>
-                <Icon iconId={"ico1"}/>
-                <Icon iconId={"ico2"}/>
-                <Icon iconId={"ico3"}/>
-                <Icon iconId={"ico4"}/>
-                <Icon iconId={"ico5"}/>
+                <Icon iconId={"ico1"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
+                <Icon iconId={"ico2"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
+                <Icon iconId={"ico3"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
+                <Icon iconId={"ico4"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
+                <Icon iconId={"ico5"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
             </FlexWrapper>
             <FlexWrapper wrap={"wrap"} justify={"space-between"}>
-                <Icon iconId={"ico6"}/>
-                <Icon iconId={"ico7"}/>
-                <Icon iconId={"ico8"}/>
-                <Icon iconId={"ico9"}/>
-                <Icon iconId={"ico10"}/>
+                <Icon iconId={"ico6"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
+                <Icon iconId={"ico7"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
+                <Icon iconId={"ico8"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
+                <Icon iconId={"ico9"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
+                <Icon iconId={"ico10"} width={"120px"} height={"120px"} viewBox={"0, 0, 120, 120"}/>
             </FlexWrapper>
 
 
