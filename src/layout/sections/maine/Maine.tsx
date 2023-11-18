71a228001936b72b55ca91c98d53189732ca546b
BukreevSergey <bukreevwork2@gmail.com>
2023-11-18 16:22:09 +0200

    add adaptive for mobile for maine section

--- src/layout/sections/maine/Maine.tsx
+++ src/layout/sections/maine/Maine.tsx
@@ -4,12 +4,13 @@ import MainePhoto from "./../../../assets/images/photo-2.webp"
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {Theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 
 export const Maine = () => {
     return (
         <StyledMaine>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Sergey Bukreev</span></Name>
@@ -35,6 +36,7 @@ const StyledMaine = styled.section`
 const PhotoWrapper = styled.div`
   position: relative;
   z-index: 0;
+  margin-top: 65px;
   &::before {
     content: "";
     width: 360px;
@@ -45,6 +47,13 @@ const PhotoWrapper = styled.div`
     top: -24px;
     left: 24px;
     z-index: -1;
+    
+    @media ${Theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+      top: -17px;
+      left: 20px;
+    }
   }
 
 `
@@ -54,26 +63,28 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  margin-right: 20px;
+  
+  @media ${Theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
 `
 
-const Text = styled.h2`
-  font-family: Poppins,serif;
-  font-size: 27px;
-  font-weight: 400;
-  
-  
+const Text = styled.h1`
+  ${font({ weight: 400, Fmax: 27, Fmin: 20})}
   
 `
 const Name = styled.h2`
-font-family: Josefin Sans, serif;
-  font-size: 50px;
-  font-weight: 700;
+${font({family:"Josefin Sans, serif", weight: 700, Fmax: 50, Fmin: 36})}
+
  letter-spacing: 0.05em;
   margin: 10px 0;
   
   span {
     position: relative;
     z-index: 0;
+    white-space: nowrap;
     &::before {
       content: "";
       display: inline-block;
@@ -86,8 +97,13 @@ font-family: Josefin Sans, serif;
       
     }
   }
+  
+  @media ${Theme.media.mobile} {
+  margin: 15px 0 22px ;
+}
 `
 const SmallText = styled.h2`
-font-size: 14px ;
+  
+  font-size: 14px ;
   font-weight: 400;
 `
